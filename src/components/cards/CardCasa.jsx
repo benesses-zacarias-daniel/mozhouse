a42de318553bf7bef76835906ee1da1fd482f6af
Benesses Zacarias Daniel <benezeszdaniel@gmail.com>
2026-06-09 20:36:12 +0200

    Criei o arquivo de Casa (Card de Casa) apenas isso e adicionei alguns elementos na home fiz um mecanismo para preencher apenas com infos falsas até depois fazer o css

--- src/components/cards/CardCasa.jsx
+++ src/components/cards/CardCasa.jsx
@@ -1,21 +1,30 @@
 import Img from "../img/Img"
 
-const CardCasa = ({ srcCasa, categoria, preco, quartos, banhos, area }) => {
+const CardCasa = ({ srcCasa, categoria, preco, localizacao, nome, quartos, banhos, area }) => {
     return (
-        <div>
-            <div>
+        <div className="area_geral_card">
+            <div className="area_img_card">
                 <Img src={srcCasa} alt={srcCasa} />
-                <div>{preco}</div>
-                <div>{categoria}</div>
+                <div className="area_preco_casa">{preco} MT</div>
+                <div className="area_categoria"> {categoria}</div>
             </div>
-            <div>
-                <div>
-
+            <div className="area_info_card">
+                <div className="area_localizacao">
+                    <div className="localizacao">{localizacao}</div>
+                    <h3 className="nome_casa">{nome}</h3>
                 </div>
-                <div>
-
+                <div className="">
+                    <hr className="linha" />
+                    <div className="info_medidas">
+                        <div>{quartos}</div>
+                        <div>{banhos}</div>
+                        <div>{area}</div>
+                    </div>
                 </div>
             </div>
         </div>
     )
 }
+
+
+export default CardCasa;
--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -3,6 +3,7 @@ import style from "./Inicio.module.css";
 import Button from "../botaos/Button"
 import { FaArrowRight } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import CardCasa from "../cards/CardCasa";
 
 const Inicio = () => {
 
@@ -22,6 +23,9 @@ const Inicio = () => {
                                     <FaArrowRight />
                                 </Link>
                             </Button>
+                        </div>
+                        <div className={style.propriedades}>
+                            <CardCasa area={1} banhos={2} quartos={4} categoria={"Teste"} localizacao={"Teste Loca"} nome={"name"} preco={20000} srcCasa={"#"} />
                         </div>
                     </div>
                 </div>
