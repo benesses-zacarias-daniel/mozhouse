d284b0ac8b50fcebba8ece785d6ebd302635676d
Benesses Zacarias Daniel <benezeszdaniel@gmail.com>
2026-06-10 08:11:41 +0200

    Ajuste do caminho de Imgs

--- src/components/data/Casas.js
+++ src/components/data/Casas.js
@@ -1,4 +1,4 @@
-const Casas = [{ id: 0, preco: 10000, categoria: "Bla Bla", compra: false, srcCasa: "../../../public/img/img.png", local: "Teste local", nome: "O nome", quatos: 2, banhos: 1, area: 240 }];
+const Casas = [{ id: 0, preco: 10000, categoria: "Bla Bla", compra: false, srcCasa: "/img/img.png", local: "Teste local", nome: "O nome", quatos: 2, banhos: 1, area: 240 }];
 
 
 export default Casas;
