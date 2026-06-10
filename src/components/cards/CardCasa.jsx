1429e5b254b6185f06266f13edd4c915108fcbe9
Benesses Zacarias Daniel <benezeszdaniel@gmail.com>
2026-06-10 07:16:14 +0200

    Criei o css dos cards de casa, a pasta img em public como também o arquivo de dados Casas.js onde tem um obj com todas as infos da casa ate algo novo que é a propriedade comprar:false para informar se é alugar ou comprar (nota ainda nao criei um campo onde essa info será mostrada)

--- src/components/cards/CardCasa.jsx
+++ src/components/cards/CardCasa.jsx
@@ -1,24 +1,26 @@
+import style from "./CardCasa.module.css"
 import Img from "../img/Img"
+import { FaBath, FaDraftingCompass, FaRuler } from "react-icons/fa";
 
 const CardCasa = ({ srcCasa, categoria, preco, localizacao, nome, quartos, banhos, area }) => {
     return (
-        <div className="area_geral_card">
-            <div className="area_img_card">
+        <div className={style.area_geral_card}>
+            <div className={style.area_img_card}>
                 <Img src={srcCasa} alt={srcCasa} />
-                <div className="area_preco_casa">{preco} MT</div>
-                <div className="area_categoria"> {categoria}</div>
+                <div className={style.area_categoria}> {categoria}</div>
+                <div className={style.area_preco_casa}>{preco} MT</div>
             </div>
-            <div className="area_info_card">
-                <div className="area_localizacao">
-                    <div className="localizacao">{localizacao}</div>
-                    <h3 className="nome_casa">{nome}</h3>
+            <div className={style.area_info_card}>
+                <div className={style.area_localizacao}>
+                    <div className={style.localizacao}>{localizacao}</div>
+                    <h3 className={style.nome_casa}>{nome}</h3>
                 </div>
-                <div className="">
-                    <hr className="linha" />
-                    <div className="info_medidas">
-                        <div>{quartos}</div>
-                        <div>{banhos}</div>
-                        <div>{area}</div>
+                <div className={style.area_medidas}>
+                    <hr className={style.linha} />
+                    <div className={style.info_medidas}>
+                        <div><FaRuler />{quartos}</div>
+                        <div><FaBath />{banhos}</div>
+                        <div><FaDraftingCompass />{area}</div>
                     </div>
                 </div>
             </div>
--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -4,10 +4,9 @@ import Button from "../botaos/Button"
 import { FaArrowRight } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import CardCasa from "../cards/CardCasa";
-
+import Casas from "../data/Casas"
 const Inicio = () => {
 
-    console.log("home");
     return (
         <section className={style.area_inicio}>
             <Hero titulo={"Encontre a casa dos seus sonhos em "} foco={"Moçambique"} mens={"Acesso exclusivo às propriedades mais luxuosas e bem localizadas. Viva a excelencia imobiliária."} />
@@ -25,7 +24,7 @@ const Inicio = () => {
                             </Button>
                         </div>
                         <div className={style.propriedades}>
-                            <CardCasa area={1} banhos={2} quartos={4} categoria={"Teste"} localizacao={"Teste Loca"} nome={"name"} preco={20000} srcCasa={"#"} />
+                            <CardCasa area={Casas[0].area} banhos={Casas[0].banhos} quartos={Casas[0].quatos} categoria={Casas[0].categoria} localizacao={Casas[0].local} nome={Casas[0].nome} preco={Casas[0].preco} srcCasa={Casas[0].srcCasa} />
                         </div>
                     </div>
                 </div>
