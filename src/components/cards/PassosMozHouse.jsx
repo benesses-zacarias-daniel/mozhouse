18b4d5c5408e9dbf5484f7bbe0359fb1c359c072
Benesses Zacarias Daniel <benezeszdaniel@gmail.com>
2026-06-24 09:03:52 +0200

    Mudei o fundo de tudo a partir do index.css e terminei a estilisação mínima da home

--- src/components/cards/PassosMozHouse.jsx
+++ src/components/cards/PassosMozHouse.jsx
@@ -1,9 +1,9 @@
 import Img from "../img/Img";
 import style from "./PassosMozHouse.module.css";
 
-const PassosMozHouse = ({ titulo, minInfo, dadosPedido }) => {
+const PassosMozHouse = ({ titulo, minInfo, classeAreaGeral = style.area_geral_passos, classeAreaPasso = style.area_passos, classeAreaImg = style.area_img_passos, classePasso = style.passos, dadosPedido }) => {
     return (
-        <div className={style.area_geral_passos}>
+        <div className={classeAreaGeral}>
             <div className={style.titulo_passos}>
                 <h2>{titulo}</h2>
                 {minInfo && (
@@ -12,11 +12,11 @@ const PassosMozHouse = ({ titulo, minInfo, dadosPedido }) => {
                     </div>
                 )}
             </div>
-            <div className={style.area_passos}>
+            <div className={classeAreaPasso}>
                 {dadosPedido.map((pedido) => {
                     return (
-                        <div key={pedido.id} className={style.passos}>
-                            <div className={style.area_img_passos}>
+                        <div key={pedido.id} className={classePasso}>
+                            <div className={classeAreaImg}>
                                 <Img src={pedido.src} alt={pedido.alt} estilo={style.passos_img} />
                             </div>
                             <div className={style.info_passo}>
--- src/components/cards/PedidoMozHouse.jsx
+++ src/components/cards/PedidoMozHouse.jsx
@@ -1,9 +1,9 @@
 import style from "./PedidoMozHouse.module.css";
 
-const PedidoMozHouse = ({ titulo_pedido, desc_pedido, children }) => {
+const PedidoMozHouse = ({ titulo_pedido, desc_pedido, children, classeAreaPedido = style.area_pedido }) => {
     return (
         <div className={style.container_pedido}>
-            <div className={`${style.area_pedido}`}>
+            <div className={classeAreaPedido}>
                 <div className={style.titulo_pedido}>
                     <h2>{titulo_pedido}</h2>
                 </div>
