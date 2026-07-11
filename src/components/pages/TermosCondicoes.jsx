0e893e8523de31943d1d5a55bbaa2c964256d001
Benesses Zacarias Daniel <benezeszdaniel@gmail.com>
2026-07-11 18:24:26 +0200

    Ajuste de algum termo.

--- src/components/pages/TermosCondicoes.jsx
+++ src/components/pages/TermosCondicoes.jsx
@@ -75,9 +75,11 @@ const TermosCondicoes = () => {
 
     return (
         <section className={style.area_termos_condicoes}>
-            {/* <div className="area_btn_termos">
+            {/* 
+            <div className="area_btn_termos">
                 <Button children={"SEGURANÇA E TRANSPARÊNCIA"} estilo={style.btn_termos_} onClick={() => { }} />
-            </div> */}
+            </div> 
+            */}
             <Hero titulo={"Termos e Condições"} mens={"Bem-vindo à MozHouse. Ao utilizar a nossa plataforma, aceita os termos e condições descritos nesta página para garantir uma experiência segura e profissional."} />
             <div className={style.termosMozHouse}>
                 <Termos dadosTermos={TermosCondicoesDados} />
